33c4bbaaee7695668957df03b3879dc72f29dd5a
Senali <senalidilumika@gmail.com>
2022-11-23 23:59:33 +0530

    Home layouut

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,23 @@ export default function Home() {
         <meta name="description" content="Senali's portfolio" />
         <link rel="icon" href="/senali-logo.png" />
       </Head>
-      <h1 className="text-3xl text-red-300 font-bold underline">
-        Hello world!
-      </h1>
+      <div className="h-[100vh] mx-auto">
+        <div className="max-w-[1200px] h-[100vh] flex justify-center flex-col items-center">
+          <div className="w-1/2">
+            <h1 className="text-3xl text-red-300 font-bold underline">
+              Hello
+            </h1>
+            <h1 className="text-3xl text-red-300 font-bold underline">
+              I am
+            </h1>
+            <h1 className="text-3xl text-red-300 font-bold underline">
+              Senali
+            </h1>
+          </div>
+          <div className="bg-slate-100">
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
